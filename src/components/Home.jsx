d2f8b8bf13c7f4ba6e6f766d1e45ab3ea460b330
Drishyaat <drishyat2307@gmail.com>
2024-11-11 22:32:46 +0530

    created branch drishya

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import Wellnest from "../assets/WellNest.jpg";
 import ImageSlider from "./slider";
 import BmiCalculator from "./bmi";
 
+
 import {
   Book,
   Sparkles,
@@ -16,25 +17,33 @@ import {
 export default function Home() {
   return (
     <div className="min-h-screen bg-white-200">
-
-      <main className="container mx-auto px-4 py-12">
+      <main className="container mx-auto px-4 py-12" >
         <section className="text-center mb-20">
-
-
           <div className="bg-white container mx-auto flex flex-col justify-center items-center py-2">
-            <img src={Wellnest} alt="Wellnest" mx-auto className="w-[80%] sm:w-1/2 md:w-1/3 lg:w-1/4" />
+            <img
+              src={Wellnest}
+              alt="Wellnest"
+              className="w-[80%] sm:w-1/2 md:w-1/3 lg:w-1/4"
+            />
             <h2 className="text-2xl sm:text-2xl font-bold italic text-pink-400 mb-6">
-            Nourish your mind Nourish your soul
+              Nourish your mind Nourish your soul
             </h2>
           </div>
-
-          
-          
-
         </section>
-        <section><ImageSlider/></section>
-        <section><BmiCalculator/></section>
 
+        <section>
+          <ImageSlider />
+        </section>
+       <section><h2>hello</h2></section>  
+        {/* BmiCalculator section with background image */}
+        <div
+          className="bg-cover bg-center py-16"
+          
+        >
+          <section>
+            <BmiCalculator />
+          </section>
+        </div>
       </main>
     </div>
   );
